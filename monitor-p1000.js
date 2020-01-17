fe724041d370220df47001a54ffdc56cd5d034bb
undefined <sjx233@qq.com>
2020-01-17 19:06:35 +0800

    format date

--- monitor-p1000.js
+++ monitor-p1000.js
@@ -1,18 +1,14 @@
+const { DateTime } = require("luxon");
 const fetch = require("node-fetch");
 const { getGist, patchGist } = require("./github-api");
 
 const gistId = process.env.GIST_ID;
 if (!gistId) throw new Error(`GIST_ID is not set`);
 
-async function getProblem(pid) {
-  const res = await fetch(`https://www.luogu.com.cn/problem/${pid}?_contentOnly=1`);
-  const obj = await res.json();
-  return obj.currentData.problem;
-}
-
 (async () => {
-  const currentTime = Date.now();
-  const problem = await getProblem("P1000");
+  const res = await fetch("https://www.luogu.com.cn/problem/P1000?_contentOnly=1");
+  const currentTime = DateTime.fromHTTP(res.headers.get("Date")).toMillis();
+  const problem = (await res.json()).currentData.problem;
   const data = JSON.parse((await getGist(gistId)).files["2-data.json"].content)
     .filter(({ time }) => currentTime <= time + 86400000);
   data.unshift({
@@ -22,8 +18,8 @@ async function getProblem(pid) {
   await patchGist(gistId, {
     files: {
       "1-index.md": {
-        content: "| 时间 | AC 率 |\n|-|-|\n" + data.map(({ time, rate }) =>
-          `| ${new Date(time).toLocaleString("zh-CN", { timeZone: "Asia/Shanghai" })} | ${(rate * 100).toFixed(6)}% |\n`).join("")
+        content: "| 时间 | 通过率 |\n|-|-|\n" + data.map(({ time, rate }) =>
+          `| ${DateTime.fromMillis(time).toFormat("yyyy-LL-dd hh:mm:ss")} | ${(rate * 100).toFixed(6)}% |\n`).join("")
       },
       "2-data.json": {
         content: JSON.stringify(data, undefined, 2) + "\n"
