4ad29070aeb371dadb48ff3d20662158a389b82f
undefined <sjx233@qq.com>
2020-01-17 19:20:43 +0800

    extract constants

--- monitor-p1000.js
+++ monitor-p1000.js
@@ -5,11 +5,14 @@ const { getGist, patchGist } = require("./github-api");
 const gistId = process.env.GIST_ID;
 if (!gistId) throw new Error(`GIST_ID is not set`);
 
+const targetFile = "1-index.md";
+const dataFile = "2-data.json";
+
 (async () => {
   const res = await fetch("https://www.luogu.com.cn/problem/P1000?_contentOnly=1");
   const currentTime = DateTime.fromHTTP(res.headers.get("Date")).toMillis();
   const problem = (await res.json()).currentData.problem;
-  const data = JSON.parse((await getGist(gistId)).files["2-data.json"].content)
+  const data = JSON.parse((await getGist(gistId)).files[dataFile].content)
     .filter(({ time }) => currentTime <= time + 86400000);
   data.unshift({
     time: currentTime,
@@ -17,11 +20,11 @@ if (!gistId) throw new Error(`GIST_ID is not set`);
   });
   await patchGist(gistId, {
     files: {
-      "1-index.md": {
+      [targetFile]: {
         content: "| 时间 | 通过率 |\n|-|-|\n" + data.map(({ time, rate }) =>
           `| ${DateTime.fromMillis(time, { zone: "Asia/Shanghai" }).toFormat("yyyy-LL-dd HH:mm:ss")} | ${(rate * 100).toFixed(6)}% |\n`).join("")
       },
-      "2-data.json": {
+      [dataFile]: {
         content: JSON.stringify(data, undefined, 2) + "\n"
       }
     }
