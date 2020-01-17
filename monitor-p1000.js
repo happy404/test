355bd1ce67aa4265d073bd2b71d73bbe88240a02
undefined <sjx233@qq.com>
2020-01-17 21:53:21 +0800

    render to svg

--- monitor-p1000.js
+++ monitor-p1000.js
@@ -5,28 +5,64 @@ const { getGist, patchGist } = require("./github-api");
 const gistId = process.env.GIST_ID;
 if (!gistId) throw new Error(`GIST_ID is not set`);
 
-const targetFile = "1-index.md";
+const resultFile = "1-monitor-p1000.svg";
 const dataFile = "2-data.json";
+
+function formatTime(time) {
+  return DateTime.fromMillis(time, { zone: "Asia/Shanghai" }).toFormat("yyyy-LL-dd HH:mm:ss");
+}
+
+function render(data, currentTime) {
+  const rates = data.map(({ rate }) => rate);
+  const min = Math.min(...rates);
+  const max = Math.max(...rates);
+  const rMin = Math.max(3 * min - 2 * max, 0);
+  const rMax = Math.min(3 * max - 2 * min, 1);
+  const points = data.map(({ time, rate }) => [
+    660 - (currentTime - time) * 0.000006666666666666667,
+    428 - (rate - rMin) / (rMax - rMin) * 400
+  ]);
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="712" height="448" font-size="14" fill="transparent">
+  <g stroke="black" stroke-width="2">
+    <path d="M 64,16 l 4,-4 4,4 m -4,-4 v 416 h 608 m -4,-4 l 4,4 l -4,4" />
+    <line x1="68" y1="28" x2="72" y2="28" />
+    <line x1="84" y1="428" x2="84" y2="424" />
+    <line x1="660" y1="428" x2="660" y2="424" />
+  </g>
+  <g fill="black" dominant-baseline="central">
+    <text x="680" y="428">时间</text>
+    <text x="76" y="12">通过率</text>
+    <g text-anchor="end">
+      <text x="64" y="428">${rMin.toFixed(6)}</text>
+      <text x="64" y="28">${rMax.toFixed(6)}</text>
+    </g>
+    <g dominant-baseline="hanging">
+      <text x="84" y="432">${formatTime(currentTime - 86400000)}</text>
+      <text x="660" y="432" text-anchor="end">${formatTime(currentTime)}</text>
+    </g>
+  </g>
+  <polyline points="${points.map(point => point.join()).join(" ")}" stroke="black" stroke-width="1" />
+  <g fill="black">
+${points.map(([x, y]) => `    <circle cx="${x}" cy="${y}" r="1" />`).join("\n")}
+  </g>
+</svg>
+`;
+}
 
 (async () => {
   const res = await fetch("https://www.luogu.com.cn/problem/P1000?_contentOnly=1");
   const currentTime = DateTime.fromHTTP(res.headers.get("Date")).toMillis();
   const problem = (await res.json()).currentData.problem;
   const data = JSON.parse((await getGist(gistId)).files[dataFile].content)
-    .filter(({ time }) => currentTime <= time + 86400000);
-  data.unshift({
+    .filter(({ time }) => currentTime <= time + 86400000).reverse();
+  data.push({
     time: currentTime,
     rate: problem.totalAccepted / problem.totalSubmit
   });
   await patchGist(gistId, {
     files: {
-      [targetFile]: {
-        content: "| 时间 | 通过率 |\n|-|-|\n" + data.map(({ time, rate }) =>
-          `| ${DateTime.fromMillis(time, { zone: "Asia/Shanghai" }).toFormat("yyyy-LL-dd HH:mm:ss")} | ${(rate * 100).toFixed(6)}% |\n`).join("")
-      },
-      [dataFile]: {
-        content: JSON.stringify(data, undefined, 2) + "\n"
-      }
+      [resultFile]: { content: render(data, currentTime) },
+      [dataFile]: { content: JSON.stringify(data) + "\n" }
     }
   });
 })().catch(error => {
