f293fff64f3d4ce1b5ed769bb53cb53e9718b361
undefined <sjx233@qq.com>
2020-03-02 13:41:30 +0800

    improve rendered svg
    Signed-off-by: undefined <sjx233@qq.com>

--- monitor-p1000.js
+++ monitor-p1000.js
@@ -22,29 +22,28 @@ function render(data, currentTime) {
   const rates = data.map(({ rate }) => rate);
   const min = Math.min(...rates);
   const max = Math.max(...rates);
-  const rMin = Math.max(3 * min - 2 * max, 0);
-  const rMax = Math.min(3 * max - 2 * min, 1);
   const points = data.map(({ time, rate }) => [
     660 - (currentTime - time) * 576 / timeout,
-    428 - (rate - rMin) / (rMax - rMin) * 400
+    428 - (rate - min) / (max - min) * 400
   ]);
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="712" height="448" font-size="14" fill="transparent">
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="712" height="464" font-size="14" fill="transparent">
   <g stroke="black" stroke-width="2">
-    <path d="M 64,16 l 4,-4 4,4 m -4,-4 v 416 h 608 m -4,-4 l 4,4 l -4,4" />
+    <path d="M 64,16 l 4,-4 4,4 m -4,-4 v 432 h 608 m -4,-4 l 4,4 l -4,4" />
     <line x1="68" y1="28" x2="72" y2="28" />
-    <line x1="84" y1="428" x2="84" y2="424" />
-    <line x1="660" y1="428" x2="660" y2="424" />
+    <line x1="68" y1="428" x2="72" y2="428" />
+    <line x1="84" y1="444" x2="84" y2="440" />
+    <line x1="660" y1="444" x2="660" y2="440" />
   </g>
   <g fill="black" dominant-baseline="central">
-    <text x="680" y="428">时间</text>
+    <text x="680" y="444">时间</text>
     <text x="76" y="12">通过率</text>
     <g text-anchor="end">
-      <text x="64" y="428">${rMin.toFixed(6)}</text>
-      <text x="64" y="28">${rMax.toFixed(6)}</text>
+      <text x="64" y="428">${min.toFixed(6)}</text>
+      <text x="64" y="28">${max.toFixed(6)}</text>
     </g>
     <g dominant-baseline="hanging">
-      <text x="84" y="432">${formatTime(currentTime - timeout)}</text>
-      <text x="660" y="432" text-anchor="end">${formatTime(currentTime)}</text>
+      <text x="84" y="448">${formatTime(currentTime - timeout)}</text>
+      <text x="660" y="448" text-anchor="end">${formatTime(currentTime)}</text>
     </g>
   </g>
   <polyline points="${points.map(point => point.join()).join(" ")}" stroke="black" stroke-width="0.5" />
@@ -65,8 +64,7 @@ ${points.map(([x, y]) => `    <circle cx="${x}" cy="${y}" r="0.5" />`).join("\n"
   }, 5);
   const gist = await autoRetry(() => getGist(gistId).then(res => res.json()), 5);
   const data = JSON.parse(gist.files[dataFile].content)
-    .filter(entry => time <= entry.time + timeout)
-    .filter(entry => entry.rate > 0.1 && entry.rate < 0.9);
+    .filter(entry => time <= entry.time + timeout);
   const entry = {
     time,
     rate: problem.totalAccepted / problem.totalSubmit
