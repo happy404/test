520fe2f97fdb3f5e3e6320da19f23e6b0ca8c974
undefined <sjx233@qq.com>
2020-06-07 07:05:38 +0800

    Update auto-punch.js

--- auto-punch.js
+++ auto-punch.js
@@ -10,7 +10,6 @@ if (!clientId) throw new Error(`LUOGU_CLIENT_ID is not set`);
 (async () => {
   const obj = await autoRetry(() => fetchLuogu("/index/ajax_punch").then(res => res.json()), 5);
   const status = obj.code;
-  if (status < 200 || status >= 300) throw new Error(`failed to punch (${status}): ${obj.message}`);
   if (status === 200) console.log(htmlToText.fromString(obj.more.html));
-  else console.error(obj.message);
+  else throw new Error(`failed to punch (${status}): ${obj.message}`);
 })().catch(handleError);
